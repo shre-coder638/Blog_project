8f166e99a5adf34489406d09af47e0ae575723d6
shre-coder638 <shankarshreyash79@gmail.com>
2026-04-02 14:30:06 +0530

    Project upload

---
+++ blog_app/serializer.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Post
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source='author.username')
+    class Meta:
+        model = Post
+        fields = '_all_'
