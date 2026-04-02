5d08704676daccdaa1a878117d96ce1c78ac781a
shre-coder638 <shankarshreyash79@gmail.com>
2026-04-02 14:55:24 +0530

    Add permissions and views for post management with JWT authentication

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Post
+from .serializer import PostSerializer
+from rest_framework.permissions import IsAuthenticated
+from .permission import IsAuthorOrReadOnly
+
 
 # Create your views here.
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
