259f7827cae4e9d35ffb24fc9a9083269cafe217
shre-coder638 <shankarshreyash79@gmail.com>
2026-04-03 10:59:15 +0530

    Add initial URL routing and JWT authentication settings for the blog application

--- blog_project/urls.py
+++ blog_project/urls.py
@@ -20,7 +20,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('blog_app_urls')),
+    path('api/', include('blog_app.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
